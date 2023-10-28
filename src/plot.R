e966850041f026607a1f32e0e134553420e8ba8a
Dennis Psaroudakis <psaroudakis@ipk-gatersleben.de>
2023-10-28 16:05:03 +0200

    improve plots

--- src/plot.R
+++ src/plot.R
@@ -1,4 +1,5 @@
 library(stringr)
+library(zoo)
 files <- list.files(path=".", pattern="*.csv", full.names=F, recursive=FALSE)
 best.score = 0
 best.episode = ""
@@ -12,15 +13,22 @@ for(f in files) {
     best.score = ep.score
     best.episode = ep.name
   }
-  l[8:13] = log(l[8:13])
-  y.max = max(l[8:13])
-  y.min = min(l[8:13])
+  l[8:11] = log(l[8:11])
+  y.max = max(l[8:11])
+  y.min = min(l[8:11])
   
-  pdf(paste0(ep.name, ".pdf"), 7, 14)
-  par(mfrow=c(9,1), mar=c(2, 4.5, 0.2, 0))
+  pdf(paste0("pdfs/", ep.name, ".pdf"), 7, 14)
+  par(mfrow=c(10,1), mar=c(2, 4.5, 0.2, 0))
   plot(l$temperature ~ l$time, type="l", ylab="Temperature")
   plot(l$humidity ~ l$time, type="l", ylab="Humidity")
   plot(l$sun_intensity ~ l$time, type="l", ylab="Sun Intensity")
+  legend("topleft", pch=c(1,4),
+         legend = c("Open Stomata", "Close Stomata"))
+  # Mark stomata openings and closings
+  actions = l[l$action == "OPEN_STOMATA",]
+  points(actions$time, actions$sun_intensity, pch=1)
+  actions = l[l$action == "CLOSE_STOMATA",]
+  points(actions$time, actions$sun_intensity, pch=4)
   plot(l$precipitation ~ l$time, type="l", ylab="Precipitation")
   plot(l$water_pool ~ l$time, type="l", ylab="Internal Water Pool")
   lines(l$max_water_pool ~ l$time, col="grey", lty=2)
@@ -28,21 +36,21 @@ for(f in files) {
   plot(l$accessible_nitrate ~ l$time, type="l", ylab="Accessible Nitrate")
   
   plot(l$leaf_percent ~ l$time, type="n", ylim=c(0, 100), ylab="Growth Percentages")
-  lines(l$leaf_percent ~ l$time, col="darkgreen")
-  lines(l$stem_percent ~ l$time, col="green")
-  lines(l$root_percent ~ l$time, col="brown")
-  lines(l$seed_percent ~ l$time, col="red")
-  lines(l$starch_percent ~ l$time, col="black")
+  lines(rollmean(l$leaf_percent, k = 5, fill=0) ~ l$time, col="darkgreen")
+  lines(rollmean(l$stem_percent, k = 5, fill=0) ~ l$time, col="green")
+  lines(rollmean(l$root_percent, k = 5, fill=0) ~ l$time, col="brown")
+  lines(rollmean(l$starch_percent, k = 5, fill=0) ~ l$time, col="black")
   
   plot(l$leaf_biomass ~ l$time, type="n", ylim=c(y.min, y.max), ylab="log(Biomass)")
   lines(l$leaf_biomass ~ l$time, col="darkgreen")
   lines(l$stem_biomass ~ l$time, col="green")
   lines(l$root_biomass ~ l$time, col="brown")
   lines(l$seed_biomass ~ l$time, col="red")
-  lines(l$starch_pool ~ l$time, col="black")
-  lines(l$max_starch_pool ~ l$time, col="grey", lty=2)
   legend("topleft", col=c("darkgreen", "green", "brown", "red", "black", "grey"),
          legend = c("Leaf", "Stem", "Root", "Seed", "Starch Pool", "Max Starch Pool"), lty=c(rep(1, 5), 2), lwd=2)
+  plot(l$starch_pool ~ l$time, col="black", type="l", ylab="Starch Pool")
+  lines(l$max_starch_pool ~ l$time, col="grey", lty=2)
+  
   dev.off()
 }
 print(paste0("Best episode: ", best.episode, " with score ", best.score))
